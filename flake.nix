fa9d1d11028ef72f40783c0cc9da0021f5dbb975
John Shaffer <john@insilica.co>
2023-09-25 12:30:14 -0500

    Update hdt-cpp source
    The last release is 4 years old, so we'll use the latest git master.

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       with import nixpkgs { inherit system; };
       let
-        hdt-version = "1.3.3";
+        hdt-version = "d9ae092bb37d9fe85558dfb3edfe0bb6ddddf41a";
         hdt-cpp = stdenv.mkDerivation {
           pname = "hdt";
           version = hdt-version;
@@ -17,8 +17,8 @@
           src = fetchFromGitHub {
             owner = "rdfhdt";
             repo = "hdt-cpp";
-            rev = "v${hdt-version}";
-            sha256 = "1vsq80jnix6cy78ayag7v8ajyw7h8dqyad1q6xkf2hzz3skvr34z";
+            rev = "${hdt-version}";
+            sha256 = "sha256-n9bTZq2BXWpZ9mKsANTO2QXzED/AG4HGxGG1p5HdtE4=";
           };
 
           buildInputs = [ zlib serd ];
